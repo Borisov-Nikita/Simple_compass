602f271a3d6f7ed4f8352899cfca0a13bbb4dbd2
Borisov Nikita <borisov-ta@yandex.ru>
2023-09-15 11:18:52 +0400

    Added correction of gravity and geomagnetic values

--- app/src/main/java/nik/borisov/simplecompass/SampleCompass.kt
+++ app/src/main/java/nik/borisov/simplecompass/SampleCompass.kt
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.abs
 
 class SampleCompass(
     private val context: Context,
@@ -44,15 +45,54 @@ class SampleCompass(
             sensorManager.registerListener(
                 this,
                 sensor,
-                SensorManager.SENSOR_DELAY_NORMAL
+                SensorManager.SENSOR_DELAY_GAME
             )
         }
     }
 
     private fun updateValues(source: FloatArray, newValues: FloatArray) {
         for (index in source.indices) {
-            source[index] = newValues[index]
+            when (abs(source[index] - newValues[index])) {
+                in 0f..1f -> {
+                    source[index] = source[index].calculateValueWithCorrection(
+                        CORRECTION_VERY_SLOW,
+                        newValues[index]
+                    )
+                }
+
+                in 1f..3f -> {
+                    source[index] = source[index].calculateValueWithCorrection(
+                        CORRECTION_SLOW,
+                        newValues[index]
+                    )
+                }
+
+                in 3f..5f -> {
+                    source[index] = source[index].calculateValueWithCorrection(
+                        CORRECTION_NORMAL,
+                        newValues[index]
+                    )
+                }
+
+                in 5f..10f -> {
+                    source[index] = source[index].calculateValueWithCorrection(
+                        CORRECTION_FAST,
+                        newValues[index]
+                    )
+                }
+
+                else -> {
+                    source[index] = source[index].calculateValueWithCorrection(
+                        CORRECTION_VERY_FAST,
+                        newValues[index]
+                    )
+                }
+            }
         }
+    }
+
+    private fun Float.calculateValueWithCorrection(correction: Float, newValue: Float): Float {
+        return (this * correction + (1 - correction) * newValue)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -82,5 +122,14 @@ class SampleCompass(
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    companion object {
+
+        private const val CORRECTION_VERY_SLOW = 0.99f
+        private const val CORRECTION_SLOW = 0.97f
+        private const val CORRECTION_NORMAL = 0.95f
+        private const val CORRECTION_FAST = 0.93f
+        private const val CORRECTION_VERY_FAST = 0.9f
     }
 }
